6e91c624305540d9dd3dd46acb3bf819294006ff
zsqosos <zsqos@hotmail.com>
2017-06-21 21:10:01 +0800

    get list data

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -14,7 +14,17 @@
       </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌曲推荐</h1>
-        <ul></ul>
+        <ul>
+          <li v-for="item in discList" class="item">
+            <div class="avatar-left">
+              <img width="60" height="60" :src="item.imgurl">
+            </div>
+            <div class="text-right">
+              <h2 class="name" v-html="item.creator.name"></h2>
+              <p class="description" v-html="item.dissname"></p>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -22,23 +32,32 @@
 
 <script>
 import slider from 'base/slider/slider'
-import { getRecommend } from 'api/recommend'
+import { getRecommend, getDiscList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
 
 export default {
   data() {
     return {
-      recommends: []
+      recommends: [],
+      discList: []
     }
   },
   created() {
     this._getRecommend()
+    this._getDiscList()
   },
   methods: {
     _getRecommend() {
       getRecommend().then(res => {
         if (res.code === ERR_OK) {
           this.recommends = res.data.slider
+        }
+      })
+    },
+    _getDiscList() {
+      getDiscList().then(res => {
+        if (res.code === ERR_OK) {
+          this.discList = res.data.list
         }
       })
     }
@@ -50,6 +69,7 @@ export default {
 </script>
 
 <style lang="stylus">
+  @import "~common/stylus/variable"
   .recommend
     position: fixed
     width: 100%
@@ -73,4 +93,30 @@ export default {
       .recommend-list
         .list-title
           height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
+        .item
+          display: flex
+          box-sizing: border-box
+          align-items: center
+          padding: 0 20px 20px 20px
+          .avatar-left
+            flex: 0 0 60px
+            width: 60px
+            padding-right: 20px
+          .text-right
+            display: flex
+            flex-direction: column
+            justify-content: center
+            flex: 1
+            line-height: 20px
+            overflow: hidden
+            font-size: $font-size-medium
+            .name
+              margin-bottom: 10px
+              color: $color-text
+            .description
+              color: $color-text-d
 </style>
