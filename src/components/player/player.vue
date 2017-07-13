d778cc6bc7b2b424da218ae829206f67d72012e1
MEMEME <zsqos@hotmail.com>
2017-07-13 23:41:35 +0800

    update player

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,9 +1,73 @@
 <template>
   <div class="player">
     <div v-show="fullScreen" class="normal-player">
-      播放器
+      <div class="background">
+        <img height="100%" width="100%" :src="dsa">
+      </div>
+      <div class="top">
+        <div class="back">
+          <i class="icon-back"></i>
+        </div>
+        <h1 class="title" v-html="currentSong.name"></h1>
+        <h1 class="subtitle" v-html="currentSong.singer"></h1>
+      </div>
+      <div class="middle">
+        <div class="middle-l">
+          <div class="cd-wrapper">
+            <div class="cd">
+              <img class="image" :src="currentSong.image">
+            </div>
+          </div>
+          <div class="playing-lyric-wrapper">
+            <div class="play-lyric">{{}}</div>
+          </div>
+        </div>
+      </div>
+      <div class="bottom">
+        <div class="dot-wrapper">
+          <span class="dot"></span>
+          <span class="dot"></span>
+        </div>
+        <div class="progress-wrapper">
+          <span class="time time-l"></span>
+          <div class="progress-bar-wrapper">
+
+          </div>
+          <span class="time time-r"></span>
+        </div>
+        <div class="operators">
+          <div class="icon i-left">
+            <i :class="iconMode"></i>
+          </div>
+          <div class="icon i-left">
+            <i class="icon-prev"></i>
+          </div>
+          <div class="icon i-center">
+            <i :class="iconMode"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon-next"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon-not-favorite icon"></i>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="mini-player"></div>
+    <div class="mini-player">
+      <div class="icon">
+        <img src="" width="40" height="40">
+      </div>
+      <div class="text">
+        <h2 class="name" v-html="currentSong.name"></h2>
+        <p class="desc" v-html="currentSong.singer"></p>
+      </div>
+      <div class="control"></div>
+      <div class="control">
+        <i class="icon-playlist"></i>
+      </div>
+    </div>
+    <audio src=""></audio>
   </div>
 </template>
 
@@ -11,8 +75,8 @@
 import { mapGetters } from 'vuex'
 
 export default {
-  mounted() {
-    console.log(this.playList)
+  created() {
+
   },
   computed: {
     ...mapGetters([
