1b081ea1b9cf9f08fc2358985ad06904c7bc359d
MEMEME <zsqos@hotmail.com>
2017-07-14 23:28:03 +0800

    update player style

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -95,13 +95,85 @@ export default {
 
   .player
     .normal-player
-      position: absolute
+      position: fixed
       left: 0
       right: 0
       top: 0
       bottom: 0
       z-index: 150
       background: $color-background
+      .background
+        position: absolute
+        left: 0
+        top: 0
+        width: 100%
+        height: 100%
+        z-index: -1
+        opacity: 0.6
+        filter: blur(20px)
+      .top
+        position: relative
+        margin-bottom: 25px
+        .back
+          position: absolute
+          top: 0
+          left: 6px
+          z-index: 50
+          .icon-back
+            display: block
+            padding: 9px
+            font-size: $font-size-large-x
+            color: $color-theme
+            transform: rotate(-90deg)
+        .title
+          width: 70%
+          margin: 0 auto
+          text-align: center
+          no-wrap()
+          font-size: $font-size-large
+          color: $color-text
+        .subtitle
+          line-height: 20px
+          text-align:　center
+          font-size: $font-size-medium
+          color: $color-text
+      .middle
+        position: fixed
+        width: 100%
+        top: 80px
+        bottom: 170px
+        white-space: nowrap
+        font-size: 0
+        .middle-l
+          display: inline-block
+          vertical-align: top
+          position: relative
+          width: 100%
+          height: 0
+          .cd-wrapper
+            position: absolute
+            left: 10%
+            top: 0
+            width: 80%
+            height: 100%
+            .cd
+              width: 100%
+              height: 100%
+              box-sizing: border-box
+              border: 10px solid rgba(255, 255, 255, 0.1)
+              border-radius: 50%
+              &.play
+                animation: rotate 20s linear infinite
+              &.pause
+                animation-play-state: paused
+              .image
+                position: absolute
+                left: 0
+                top: 0
+                width: 100%
+                height: 100%
+                border-radius: 50%
+
     .mini-player
       display: flex
       align-items: center
