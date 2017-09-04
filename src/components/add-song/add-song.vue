f49fe58eea4338bc87610ba28cd05293d4cba54f
zsqosos <zsqos@hotmail.com>
2017-09-04 20:47:57 +0800

    add-song component

---
+++ src/components/add-song/add-song.vue
@@ -0,0 +1,84 @@
+<template>
+  <transition name="slide">
+    <div v-show="showFlag" class="add-song" @click.stop>
+      <div class="header">
+        <h1 class="title">添加歌曲到列表</h1>
+        <div class="close" @click="hide">
+          <i class="icon-close"></i>
+        </div>
+      </div>
+      <div class="search-box-wrapper">
+
+      </div>
+      <div class="shortcut">
+        <div class="list-wrapper">
+          <div class="list-scroll">
+            <div class="list-inner">
+
+            </div>
+          </div>
+          <div class="list-scroll">
+            <div class="list-inner">
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="search-result">
+
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showFlag: false
+    }
+  },
+  methods: {
+    show() {
+      this.showFlag = true
+    },
+    hide() {
+      this.showFlag = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+  .add-song
+    position: fixed
+    top: 0
+    bottom: 0
+    width: 100%
+    z-index: 200
+    background: $color-background
+    &.slide-enter-active, &.slide-leave-active
+      transition: all 0.3s
+    &.slide-enter, &.slide-leave-to
+      transform: translate3d(100%, 0, 0)
+    .header
+      position: relative
+      height: 44px
+      text-align: center
+      .title
+        line-height: 44px
+        font-size: $font-size-large
+        color: $color-text
+      .close
+        position: absolute
+        top: 0
+        right: 0
+        .icon-close
+          display: block
+          padding: 12px
+          font-size: 20px
+          color: $color-theme
+</style>
--- src/components/playlist/playlist.vue
+++ src/components/playlist/playlist.vue
@@ -26,7 +26,7 @@
           </transition-group>
         </scroll>
         <div class="list-operate">
-          <div class="add">
+          <div class="add" @click.stop="showAddSong">
             <i class="icon-add"></i>
             <span class="text">添加歌曲到队列</span>
           </div>
@@ -36,6 +36,7 @@
         </div>
       </div>
       <confirm ref="confirm" @confirm="confirmClear" text="是否清空播放列表" confirmBtnText="清空"></confirm>
+      <add-song ref="addSong"></add-song>
     </div>
   </transition>
 </template>
@@ -46,6 +47,7 @@ import Scroll from 'base/scroll/scroll'
 import { playMode } from 'common/js/config'
 import Confirm from 'base/confirm/confirm'
 import { playerMixin } from 'common/js/mixin'
+import AddSong from 'components/add-song/add-song'
 
 export default {
   mixins: [playerMixin],
@@ -105,6 +107,9 @@ export default {
     showConfirm() {
       this.$refs.confirm.show()
     },
+    showAddSong() {
+      this.$refs.addSong.show()
+    },
     ...mapActions([
       'deleteSong',
       'clearSongList'
@@ -122,7 +127,8 @@ export default {
   },
   components: {
     Scroll,
-    Confirm
+    Confirm,
+    AddSong
   }
 }
 </script>
