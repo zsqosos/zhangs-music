0cb94ad2f1979abb69e77871235d2c95f87e1c14
zsqosos <zsqos@hotmail.com>
2017-09-14 23:23:02 +0800

    add prod_server

---
+++ prod_server.js
@@ -0,0 +1,59 @@
+var express = require('express')
+// var compression = require('compression')
+var axios = require('axios')
+
+var app = express()
+var port = process.env.PORT || 8001
+
+var apiRoutes = express.Router()
+
+apiRoutes.get('/getDiscList', function (req, res) {
+  var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then(response => {
+    res.json(response.data)
+  }).catch(e => {
+    console.log(e)
+  })
+})
+
+apiRoutes.get('/lyric', function (req, res) {
+  var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
+
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then(response => {
+    var ret = response.data
+    if (typeof ret === 'string') {
+      var reg = /^\w+\(({[^()]+})\)$/
+      var dataString = reg.exec(ret)
+      if(dataString) {
+        ret = JSON.parse(dataString[1])
+      }
+    }
+    res.json(ret)
+  }).catch(e => {
+    console.log(e)
+  })
+})
+
+app.use('/api', apiRoutes)
+app.use(express.static('./dist'))
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('listen at http://localhost:' + port + '\n')
+})
